9e497517513c8f94058cd8b993a99f319be771eb
rodrigo martinez <martinez.d.rodrigo@gmail.com>
2013-11-04 18:19:59 -0300

    clase 4: vistas de post y links

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,10 +2,30 @@ class PostsController < ApplicationController
 
 	#creamos la accion new
 	def new
+		@post=Post.new()
 	end
 
 	#creamos la accion CREATE
 	def create
-	render text: params[:post].inspect
+	#render text: params[:post].inspect
+	#end
+		@post=Post.new(post_params)
+
+		@post.save
+		redirect_to @post
 	end
+
+	def show
+		@post=Post.find(params[:id])
+	end
+
+	def index
+		@posts=Post.all
+	end
+
+	private
+		def post_params
+			params.require(:post).permit(:title,:text)
+		end
+	
 end

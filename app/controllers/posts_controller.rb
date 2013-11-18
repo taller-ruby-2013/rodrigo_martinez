c5e37b2f4ce856709cce1e340cb1963c9766930c
rodrigo martinez <martinez.d.rodrigo@gmail.com>
2013-11-18 19:16:51 -0300

    segunda parte clase 5: fin creacion comentarios

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,6 +18,27 @@ class PostsController < ApplicationController
 		end
 	end
 
+	def edit
+		@post=Post.find(params[:id])
+	end
+
+	def update
+		@post=Post.find(params[:id])
+
+		if @post.update(params[:post].permit(:title,:text))
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post= Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
+	end
+
 	def show
 		@post=Post.find(params[:id])
 	end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Blog::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
    root 'welcome#index'
-   resources :posts
+   resources :posts do
+      resources :comments
+    end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
